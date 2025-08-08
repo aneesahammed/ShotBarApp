bb1a67f1158ebe6097f0388078e7553145dbb700
Anees Ahammed <aneesahammed@gmail.com>
2025-08-08 23:29:01 +0100

    Initial Commit

---
+++ ShotBarApp/ShotBarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShotBarAppApp.swift
+//  ShotBarApp
+//
+//  Created by Anees Ahammed on 08/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct ShotBarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
